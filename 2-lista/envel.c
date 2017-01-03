b80ce8cd62fc3d5e00a59040769bd129e6ed5d37
Gabriele Luz <gabrieleluz@users.noreply.github.com>
2017-01-03 16:30:03 -0300

    `Envelopes`

---
+++ 2-lista/envel.c
@@ -0,0 +1,31 @@
+// #include <iostream>
+#include <stdio.h>
+// using namespace std;
+
+int main(){
+	int n = 0, k = 0, i = 0, p = 0, menor = 1000001;
+
+	scanf("%d", &n);
+	scanf("%d", &k);
+	// cin>>n>>k;
+
+	int aux[k];
+ 	for (i = 0; i < k; i++){
+		aux[i] = 0;
+	}
+
+	for (i = 0; i < n; i++){
+		scanf("%d", &p);
+		aux[p-1]		++;
+	}
+
+	for (i = 0; i < k; i++){
+		if( aux[i] < menor ){
+			menor = aux[i];
+		}
+	}
+
+	printf("%d\n", menor);
+
+	return 0;
+}
